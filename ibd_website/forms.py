c0e92bac1db3b0dd2838ed5ff2a746521f439971
m-n-zuk <magdalena.n.zuk@gmail.com>
2023-02-13 22:12:28 +0100

    register & login

---
+++ ibd_website/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from ibd_website.models import User
+
+
+class RegisterForm(forms.Form):
+
+    username = forms.CharField(label="Nazwa użytkownika")
+    password = forms.CharField(label="Hasło", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Hasło", widget=forms.PasswordInput)
+    first_name = forms.CharField(label="Imię")
+    last_name = forms.CharField(label="Nazwisko")
+    email = forms.EmailField(label="Adres email")
+    role = forms.ChoiceField(label="Kim jestem?",
+                             choices=(('patient', 'pacjentem'), ('doctor', 'lekarzem')),
+                             widget=forms.RadioSelect)
+
+    def clean(self):  # rozbudowujemy metode rodzica
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+        if password != password2:
+            raise ValidationError("Podane hasla nie sa takie same!")
+            # wyswietla sie na samej gorze, jak zrobic zeby bylo nad password?
+
+    def clean_login(self):
+        username = self.cleaned_data.get('username')
+        user = User.objects.filter(username=username)
+        if user:
+            raise ValidationError("Podana nazwa użytkownika jest juz w bazie!")
+        return username
+
+
+class PatientForm(forms.Form):
+
+    date_of_birth = forms.DateField(label="data urodzenia")
+    medical_history = forms.CharField(label="historia choroby", widget=forms.Textarea)
+    children = forms.ChoiceField(label="czy masz dzieci?",
+                                 choices=((True, 'tak'), (False, 'nie')))
+    stoma = forms.ChoiceField(label="czy masz stomie?",
+                              choices=((True, 'tak'), (False, 'nie')))
+    visible = forms.ChoiceField(label="czy chcesz być widoczna dla innych użytkowników?",
+                                choices=((True, 'tak'), (False, 'nie')))
+
+
+class DoctorForm(forms.Form):
+
+    specialization = forms.CharField(label="specjalizacja", required=True)
+    experience = forms.CharField(label="doświadczenie", widget=forms.Textarea)
+
+
+class LoginForm(forms.Form):
+
+    username = forms.CharField(label="nazwa użytkownika")
+    password = forms.CharField(label="hasło", widget=forms.PasswordInput)
--- ibd_website/views.py
+++ ibd_website/views.py
@@ -1,7 +1,131 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.views import View
+
+from ibd_website.forms import *
+from ibd_website.models import *
 
 
 class MainView(View):
     def get(self, request):
         return render(request, 'main_page.html')
+
+
+class RegisterView(View):
+    def get(self, request):
+        form = RegisterForm()
+        return render(request, 'register.html', {'form': form})
+
+    def post(self, request):
+
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+
+            # niebezpieczna operacja (C z CRUD)
+            user = User.objects.create_user(
+                username=data.get('username'),
+                first_name=data.get('first_name'),
+                last_name=data.get('last_name'),
+                email=data.get('email'),
+                password=data.get('password')
+            )
+            role = data.get('role')
+
+            return redirect(f"/register_details/{role}/{user.id}")
+
+        else:  # dodac po dodaniu walidacji
+            return render(request, 'register.html', {'form': form})
+
+
+class RegisterDetailsView(View):
+    def get(self, request, role, id):
+
+        if role == 'patient':
+            form = PatientForm()
+        if role == 'doctor':
+            form = DoctorForm()
+        return render(request, 'register_details.html', {'form': form, 'role': role})
+
+    def post(self, request, role, id):
+
+        if role == 'patient':
+            form = PatientForm(request.POST)
+
+            if form.is_valid():
+
+                data = form.cleaned_data
+
+                user = User.objects.get(id=id)
+
+                patient_details = Patient.objects.create(
+                    patient=user,
+                    date_of_birth=data.get('date_of_birth'),
+                    medical_history=data.get('medical_history'),
+                    children=data.get('children'),
+                    stoma=data.get('stoma'),
+                    visible=data.get('visible')
+                )
+
+                return redirect(f"/login/")
+
+            else:  # dodac po dodaniu walidacji
+                return render(request, 'register_details.html', {'form': form, 'role': role})
+
+        if role == 'doctor':
+            form = DoctorForm(request.POST)
+
+            if form.is_valid():
+
+                data = form.cleaned_data
+
+                user = User.objects.get(id=id)
+
+                doctor_details = Doctor.objects.create(
+                    doctor=user,
+                    experience=data.get('experience'),
+                    specialization=data.get('specialization')
+                )
+
+                return redirect(f"/login/")
+
+            else:  # dodac po dodaniu walidacji
+                return render(request, 'register_details.html', {'form': form, 'role': role})
+
+
+class LoginView(View):
+    def get(self, request):
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
+
+    def post(self, request):
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+
+            username = data.get('username')
+            password = data.get('password')
+
+            # uwierzytelnienie
+            user = authenticate(username=username, password=password)
+
+            if user:
+                # logowanie
+                login(request, user)
+
+                return redirect(f"/user")
+
+            else:
+                messages.error(request, 'Błąd uwierzytelnienia. Podano nieprawidłowe poświadczenia.')
+
+                return render(request, 'login.html', {'form': form})
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect(f"/")
